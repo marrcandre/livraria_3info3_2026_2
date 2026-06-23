ad324bcdb002f113a938a4b5844896f9a42b1c74
Marco André Mendes <marcoandre@gmail.com>
2026-06-23 09:20:42 -0300

    feat: limitando os campos dos itens da compra na listagem de compras

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -1,6 +1,6 @@
 from .autor import AutorSerializer
 from .categoria import CategoriaSerializer
-from .compra import CompraSerializer
+from .compra import CompraSerializer, ItensCompraSerializer
 from .editora import EditoraSerializer
 from .livro import LivroListSerializer, LivroRetrieveSerializer, LivroSerializer
 from .user import UserRegistrationSerializer, UserSerializer
