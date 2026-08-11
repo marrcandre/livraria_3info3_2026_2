686311b006ed1c86e3ed9c915becf923fc0c47c3
Marco André Mendes <marcoandre@gmail.com>
2026-08-11 08:40:32 -0300

    feat: criação de um endpoint para atualizar compras

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from rest_framework.serializers import CharField, ModelSerializer, SerializerMethodField
 
 from core.models import Compra, ItensCompra
@@ -39,13 +40,22 @@ class CompraCreateUpdateSerializer(ModelSerializer):
         model = Compra
         fields = ('usuario', 'itens')
 
+    @transaction.atomic
     def create(self, validated_data):
         itens_data = validated_data.pop('itens')
         compra = Compra.objects.create(**validated_data)
         for item_data in itens_data:
             ItensCompra.objects.create(compra=compra, **item_data)
-        compra.save()
         return compra
+
+    @transaction.atomic
+    def update(self, compra, validated_data):
+        itens_data = validated_data.pop('itens', None)
+        if itens_data is not None:
+            compra.itens.all().delete()
+            for item_data in itens_data:
+                ItensCompra.objects.create(compra=compra, **item_data)
+        return super().update(compra, validated_data)
 
 
 class CompraSerializer(ModelSerializer):
